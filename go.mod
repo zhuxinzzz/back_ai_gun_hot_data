3150144628c4ff3693722a8daf0f778ab779f5fa
zhuxin <san_nicola@163.com>
2025-08-28 18:24:20 +0800

    feat(service): Adds a common remote service toolkit
    - Added two new subpackages, gmgn_service and remote_service. - gmgn_service: No implementation is available, reserve common service interfaces
    - remote_service: Provides a resty-based HTTP client toolkit
    - Add resty dependencies in go.mod and update other dependency versions

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module back_ai_gun_data
 go 1.25
 
 require (
+	github.com/go-resty/resty/v2 v2.16.5
 	github.com/rabbitmq/amqp091-go v1.10.0
 	github.com/redis/go-redis/v9 v9.12.1
 	github.com/sirupsen/logrus v1.9.3
@@ -11,5 +12,6 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 )
---
+++ service/remote_service/remote_service.go
@@ -0,0 +1,29 @@
+package remote_service
+
+import (
+	"back_ai_gun_data/pkg/lr"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
+// 扁平化HTTP客户端 - 直接使用，不包装
+var httpClient = resty.New().
+	SetTimeout(30 * time.Second).
+	SetRetryCount(3).
+	SetRetryWaitTime(1 * time.Second).
+	SetRetryMaxWaitTime(5 * time.Second)
+
+func Init() {
+	// 设置响应处理
+	httpClient.OnAfterResponse(func(client *resty.Client, resp *resty.Response) error {
+		if resp.StatusCode() >= 400 {
+			lr.E().Errorf("HTTP error: %d - %s", resp.StatusCode(), resp.String())
+		}
+		return nil
+	})
+}
+
+func GetCli() *resty.Client {
+	return httpClient
+}
